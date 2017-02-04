7bbf98a017fb975ff498e8c1b8b28d4a867aca00
d9magai <kumagai@sencorp.co.jp>
2017-02-04 01:04:53 +0000

    lock file

--- scripts/vagrant-build.sh
+++ scripts/vagrant-build.sh
@@ -54,6 +54,8 @@ dependencies="maven mysql-server openjdk-8-jdk-headless libcurl4-openssl-dev ngi
 mkdir -p ${basedir}
 cd ${basedir}
 
+if [[ ! -f /etc/vagrant_installing_updates_and_dependencies ]]; then
+  
 ## dependencies
 echo "==== Installing Updates and Dependencies ===="
 export DEBIAN_FRONTEND=noninteractive
@@ -68,6 +70,11 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 echo "apt-get install dependencies"
 apt-get -y install ${dependencies}
 
+date > /etc/vagrant_installing_updates_and_dependencies
+fi
+
+
+if [[ ! -f /etc/fetching_installing_tornado_web_server ]]; then
 echo "==== Fetching/Installing Tornado Web Server ===="
 cd /tmp
 wget ${tornado_url}
@@ -77,6 +84,10 @@ python3 setup.py install
 
 pip3 install wikipedia
 
+date > /etc/fetching_installing_tornado_web_server
+fi
+
+if [[ ! -f /etc/build_and_install_mysql-connector-python ]]; then
 ## build and install mysql-connector-python
 cd /tmp
 wget https://github.com/mysql/mysql-connector-python/archive/2.1.3.zip
@@ -84,12 +95,18 @@ unzip 2.1.3.zip
 cd mysql-connector-python-2.1.3
 python3 setup.py install
 
+date > /etc/build_and_install_mysql-connector-python
+fi
 
+if [[ ! -f /etc/mysql_setup ]]; then
 ## mysql setup
 echo "==== Setting up MySQL ===="
 mysqladmin -u ${mysql_user} -p${mysql_pass} create freeciv_web
 mysql -u ${mysql_user} -p${mysql_pass} freeciv_web < ${basedir}/freeciv-web/src/main/webapp/meta/private/metaserver.sql
+date > /etc/mysql_setup
+fi
 
+if [[ ! -f /etc/mysql_configuration_files ]]; then
 # configuration files
 dos2unix ${basedir}/scripts/configuration.sh.dist
 sed -e "s/MYSQL_USER=root/MYSQL_USER=${mysql_user}/" -e "s/MYSQL_PASSWORD=changeme/MYSQL_PASSWORD=${mysql_pass}/" ${basedir}/scripts/configuration.sh.dist > ${basedir}/scripts/configuration.sh
@@ -100,12 +117,18 @@ cp ${basedir}/freeciv-web/src/main/webapp/META-INF/context.xml.dist ${basedir}/f
 dos2unix ${basedir}/scripts/configuration.sh.dist
 sed -e "s/MYSQL_USER=root/MYSQL_USER=${mysql_user}/" -e "s/MYSQL_PASSWORD=changeme/MYSQL_PASSWORD=/" ${basedir}/scripts/configuration.sh.dist > ${basedir}/scripts/configuration.sh
   
+date > /etc/mysql_configuration_files
+fi
 
+if [[ ! -f /etc/building_freeciv ]]; then
 echo "==== Building freeciv ===="
 dos2unix ${basedir}/freeciv/freeciv-web.project
 cd ${basedir}/freeciv && sudo -Hu vagrant ./prepare_freeciv.sh
 cd freeciv && sudo -u vagrant make install
+date > /etc/building_freeciv
+fi
 
+if [[ ! -f /etc/building_freeciv-web ]]; then
 echo "==== Building freeciv-web ===="
 cd ${basedir}/scripts/freeciv-img-extract/ && ./setup_links.sh && ./sync.sh
 cd /var/lib/tomcat8 && chmod -R 777 webapps logs && setfacl -d -m g::rwx webapps && chown -R www-data:www-data webapps/
@@ -114,8 +137,13 @@ cd ${basedir}/scripts && ./sync-js-hand.sh
 cd ${basedir}/freeciv-web && sudo -u vagrant ./setup.sh
 cp flyway.properties.dist flyway.properties
 sudo -u vagrant mvn compile flyway:migrate
+date > /etc/building_freeciv-web
+fi
 
 echo "=============================="
+
+
+if [[ ! -f /etc/service_settings ]]; then
 
 cp ${basedir}/pbem/settings.ini.dist ${basedir}/pbem/settings.ini
 
@@ -123,7 +151,10 @@ service nginx stop
 rm /etc/nginx/sites-enabled/default
 sed -i.bak -e "s/php7/php\/php7.0/" ${basedir}/publite2/nginx.conf 
 cp ${basedir}/publite2/nginx.conf /etc/nginx/
+date > /etc/service_settings
+fi
 
+if [[ ! -f /etc/add_freeciv-web_scripts_to_path ]]; then
 # add Freeciv-web scripts to path
 export PATH=$PATH:/vagrant/scripts
 echo 'export PATH=$PATH:/vagrant/scripts' >> /home/vagrant/.bashrc
@@ -135,7 +166,10 @@ if [ -d "/vagrant/" ]; then
 else
   echo "Freeciv-web installed. Please start it manually."
 fi
+date > /etc/add_freeciv-web_scripts_to_path
+fi
 
+if [[ ! -f /etc/installing_casperjs_for_testing ]]; then
 echo "============================================"
 echo "Installing CasperJS for testing"
 cd ${basedir}/tests/
@@ -143,6 +177,8 @@ wget ${casperjs_url}
 unzip -qo 1.1.3
 cd casperjs-casperjs-cd78443
 ln -sf `pwd`/bin/casperjs /usr/local/bin/casperjs
+date > /etc/installing_casperjs_for_testing
+fi
 
 echo "Start testing of Freeciv-web using CasperJS:"
 cd ${basedir}/tests/
