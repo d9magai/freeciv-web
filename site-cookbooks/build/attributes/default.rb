12e8cccbd34a7b3b1a7f840f59062f8e44201e6e
d9magai <kumagai@sencorp.co.jp>
2017-04-30 15:39:07 +0100

    modify build cookbook

--- site-cookbooks/build/attributes/default.rb
+++ site-cookbooks/build/attributes/default.rb
@@ -1,3 +1,4 @@
 ## base settings
 default['freeciv-web']['path'] = '/vagrant/freeciv-web'
+default['freeciv']['path'] = '/vagrant/freeciv'
 
--- site-cookbooks/build/recipes/default.rb
+++ site-cookbooks/build/recipes/default.rb
@@ -2,6 +2,7 @@ execute 'build' do
   user 'vagrant'
   cwd node['freeciv-web']['path']
   command <<-EOH
+    cp #{node['freeciv']['path']}/freeciv/data/scenarios/*.sav /var/lib/tomcat8/webapps/data/savegames/
     mvn package
     cp target/freeciv-web.war /var/lib/tomcat8/webapps/ROOT.war
     date >> src/main/webapp/build.txt
